969354284fb6debb2385690feb16ab04af3b83e4
huang huiliang <spiritsaway@outlook.com>
2021-12-25 15:23:53 +0800

    Revert "添加test"
    This reverts commit 3448d45f2aed2e7471f4ec89000e35a9796ad3ce.

--- include/task_channel.h
+++ include/task_channel.h
@@ -12,58 +12,7 @@ namespace spiritsaway::concurrency
 {
 	// T for task type
 	// channel_type T::channel_id()
-	const std::uint32_t queue_size = 16;
 	template <typename T>
-	class simple_queue
-	{
-	protected:
-		std::deque<task_ptr> _queue;
-		std::mutex _mutex;
-	public:
-		simple_queue(const simple_queue& other) = delete;
-		simple_queue& operator=(const simple_queue& other) = delete;
-		void push_fromt(T&& data)
-		{
-			std::lock_guard<std::mutex> cur_lock;
-			_queue.push_front(std::forward<T>(data));
-			return;
-		}
-		void push_back(T&& data)
-		{
-			std::lock_guard<std::mutex> cur_lock;
-			_queue.push_back(std::forward<T>(data));
-		}
-		bool pop_front(T& dest)
-		{
-			std::lock_guard<std::mutex> cur_lock;
-			if(_queue.empty())
-			{
-				return false;
-			}
-			else
-			{
-				dest = _queue.front();
-				_queue.pop_front();
-				return true;
-			}
-		}
-		bool pop_back(T& dest)
-		{
-			std::lock_guard<std::mutex> cur_lock;
-			if(_queue.empty())
-			{
-				return false;
-			}
-			else
-			{
-				dest = _queue.back();
-				_queue.pop_back();
-				return true;
-			}
-		}
-
-	};
-	template <typename T,  template<typename T2> typename con_queue>
 	class task_channels
 	{
 	public:
@@ -71,64 +20,66 @@ namespace spiritsaway::concurrency
 		using task_ptr = std::shared_ptr<T>;
 		struct task_queue
 		{
-			con_queue<task_ptr> _queue;
-			std::atomic<std::uint32_t> executor_id = 0;
-			bool claim(std::uint32_t new_exec_id)
-			{
-				if(new_exec_id == 0)
-				{
-					return false;
-				}
-				while(executor_id == 0)
-				{
-					if(executor_id.compare_exchange_strong(0, new_exec_id))
-					{
-						return true;
-					}
-				}
-				return false;
-			}
-			bool renounce(std::uint32_t pre_exec_id)
-			{
-				while(executor_id == pre_exec_id)
-				{
-					if(executor_id.compare_exchange_strong(pre_exec_id, 0))
-					{
-						return true;
-					}
-				}
-				return false;
-			}
+			std::uint32_t executor_id = 0;
+			std::deque<task_ptr> _queue;
 		};
 	protected:
-		std::array<task_queue, queue_size> _tasks_by_channel;
+		std::unordered_map<channel_type, std::shared_ptr<task_queue>> _tasks_by_channel;
+		std::shared_ptr<task_queue> _tasks_without_channel;
 		std::mutex _task_mutex;
+		const channel_type default_channel_id;
 		std::atomic<std::uint32_t> add_task_count;
 		std::atomic<std::uint32_t> run_task_count;
 		std::atomic<std::uint32_t> finish_task_count;
 	protected:
 
-		task_queue& queue_for_channel(const channel_type& cur_channel)
+		
+		bool is_default_channel(const channel_type& channel_id) const
 		{
-			return _tasks_by_channel[std::hash<channel_type>()(cur_channel) % _tasks_by_channel.size()];
+			return channel_id != default_channel_id;
 		}
-
 	public:
 		void add_task(task_ptr task, bool front = false)
 		{
 			auto cur_channel_id = task->channel_id();
-			auto& cur_queue = queue_for_channel(cur_channel_id);
-			if(front)
+			std::lock_guard<std::mutex> task_lock(_task_mutex);
+
+			if(!is_default_channel(cur_channel_id))
 			{
-				cur_queue._queue.push_front(task);
+				auto cur_channel_iter = _tasks_by_channel.find(cur_channel_id);
+				if(cur_channel_iter == _tasks_by_channel.end())
+				{
+
+					auto cur_task_channel = std::make_shared<task_queue>();
+					cur_task_channel->_queue.push_back(task);
+				}
+				else
+				{
+					if(front)
+					{
+						cur_channel_iter->second->_queue.push_front(task);
+					}
+					else
+					{
+						cur_channel_iter->second->_queue.push_back(task);
+					}
+					
+				}
 			}
 			else
 			{
-				cur_queue._queue.push_back(task);
+				_tasks_without_channel->_queue.push_back(task);
+			}
+			add_task_count++;
+			if(add_task_count % 2000 == 0)
+			{
+				remove_empty_channels();
 			}
 			
 		}
 		task_channels()
+		: _tasks_without_channel(std::make_shared<task_queue>())
+		, default_channel_id()
 		{
 
 		}
@@ -137,38 +88,71 @@ namespace spiritsaway::concurrency
 
 		task_ptr poll_one_task(channel_type prefer_channel, std::uint32_t cur_executor_id)
 		{
-			auto& result_queue = select_task_queue(prefer_channel);
-			if(result_queue.executor_id == cur_executor_id)
+			std::lock_guard<std::mutex> task_lock(_task_mutex);
+			auto result_queue = select_task_queue(prefer_channel);
+			if(!result_queue)
 			{
-				task_ptr result;
-				if(result_queue._queue.pop_front(result))
-				{
-					run_task_count++;
-					return result;
-				}
-				else
-				{
-					result_queue.renounce(cur_executor_id);
-				}
+				return {};
 			}
-
-			for(auto& one_queue: _tasks_by_channel)
+			result_queue->executor_id = cur_executor_id;
+			auto cur_task = result_queue->_queue.front();
+			result_queue->_queue.pop_front();
+			run_task_count++;
+			return cur_task;
+			
+		}
+		std::shared_ptr<task_queue> select_task_queue(channel_type prefer_channel)
+		{
+			std::shared_ptr<task_queue> result_queue;
+			if(!is_default_channel(prefer_channel))
 			{
-				if(one_queue.claim(cur_executor_id))
+				auto cur_channel_iter = _tasks_by_channel.find(prefer_channel);
+				if(cur_channel_iter != _tasks_by_channel.end())
 				{
-					task_ptr result;
-					if(one_queue._queue.pop_front(result))
+					auto& cur_queue = cur_channel_iter->second;
+					if(!(cur_queue->_queue.empty()))
 					{
-						run_task_count++;
-						return result;
+						return cur_queue;
 					}
 					else
 					{
-						one_queue.renouce(cur_executor_id);
+						cur_queue->executor_id = 0;
 					}
+					
 				}
+			}
+
+			if(!_tasks_without_channel->_queue.empty())
+			{
+				return _tasks_without_channel;
+			}
+			for(auto& [k, v] : _tasks_by_channel)
+			{
+				if(v->executor_id || v->_queue.empty())
+				{
+					continue;
+				}
+				return v;
 			}
 			return {};
 		}
+		void remove_empty_channels()
+		{
+			auto iter_begin = _tasks_by_channel.begin();
+			while(iter_begin != _tasks_by_channel.end())
+			{
+				auto cur_iter = iter_begin;
+				iter_begin++;
+				if(cur_iter->second->_queue.empty())
+				{
+					_tasks_by_channel.erase(cur_iter);
+				}
+			}
+		}
+		virtual void finish_task(task_ptr cur_task)
+		{
+			finish_task_count++;
+		}
+
 	};
 }
