7d4cdf592b8bde704c8084cc9bc5b433c86d8cdd
huang huiliang <spiritsaway@outlook.com>
2026-02-15 22:59:07 +0800

    add_task增加front参数

--- include/task_channel.h
+++ include/task_channel.h
@@ -51,7 +51,7 @@ namespace spiritsaway::concurrency
 			return channel_id == m_default_channel_id;
 		}
 
-		void add_task_impl(task_ptr task)
+		void add_task_impl(task_ptr task, bool front)
 		{
 			auto cur_channel_id = task->channel_id();
 
@@ -60,11 +60,26 @@ namespace spiritsaway::concurrency
 				// 计算channel的哈希值，确定要使用的bucket
 				std::size_t bucket_index = hash_channel(cur_channel_id);
 				auto &task_queue = m_task_buckets[bucket_index];
-				task_queue.queue.push_back(task);
+				if (front)
+				{
+					task_queue.queue.push_front(task);
+				}
+				else
+				{
+					task_queue.queue.push_back(task);
+				}
+				
 			}
 			else
 			{
-				m_tasks_without_channel.queue.push_back(task);
+				if(front)
+				{
+					m_tasks_without_channel.queue.push_front(task);
+				}
+				else
+				{
+					m_tasks_without_channel.queue.push_back(task);
+				}
 			}
 			m_add_task_count++;
 		}
@@ -138,16 +153,16 @@ namespace spiritsaway::concurrency
 		}
 
 	public:
-		void add_task(task_ptr task)
+		void add_task(task_ptr task, bool front = false)
 		{
 			if (threading)
 			{
 				std::lock_guard<std::mutex> task_lock(m_task_mutex);
-				add_task_impl(task);
+				add_task_impl(task, front);
 			}
 			else
 			{
-				add_task_impl(task);
+				add_task_impl(task, front);
 			}
 		}
 
